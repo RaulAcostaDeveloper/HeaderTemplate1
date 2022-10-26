ebea95013e142dcc7d0a5220f6bdbbbf206d0f36
Raúl <raulacostadeveloper@outlook.com>
2022-10-26 17:15:53 -0500

    Un header muy elavorado

---
+++ js/Menu.js
@@ -0,0 +1,132 @@
+let menu = document.getElementById('menu');
+let interiorMenu = document.getElementById('interiorMenu');
+let pers1 = document.getElementById('elPersistente1');
+let pers2 = document.getElementById('elPersistente2');
+let pers3 = document.getElementById('elPersistente3');
+
+const abrirMenu = ()=>{
+   menu.style.display='block';
+   interiorMenu.style.animationName="openMenu";
+   interiorMenu.style.animationDuration="500ms";
+   interiorMenu.style.animationFillMode="initial";
+   pers1.style.display = "block";
+   pers2.style.display = "block";
+   pers3.style.display = "block";
+ }
+ const cerrarMenu = ()=>{
+   interiorMenu.style.animationName="closeMenu";
+   interiorMenu.style.animationFillMode="initial";
+   interiorMenu.style.animationDuration="500ms";
+   pers1.style.display = "none";
+   pers2.style.display = "none";
+   pers3.style.display = "none";
+   setTimeout(() => {      
+      menu.style.display='none';
+   }, 500);
+ }
+//  Sub Menús
+
+let MyBusinessSubMenuComp = document.getElementById('MyBusinessSubMenuComp');
+let MyBusinessSubMenuCompArrow = document.getElementById('MyBusinessSubMenuCompArrow');
+let isOpenMyBusinessSubMenuComp = false;
+
+let GrowthmarketingSubMenuComp = document.getElementById('GrowthmarketingSubMenuComp');
+let GrowthmarketingSubMenuCompArrow = document.getElementById('GrowthmarketingSubMenuCompArrow');
+let isOpenGrowthmarketingSubMenuComp = false;
+
+let AnalyticsSubMenuComp = document.getElementById('AnalyticsSubMenuComp');
+let AnalyticsSubMenuCompArrow = document.getElementById('AnalyticsSubMenuCompArrow');
+let isOpenAnalyticsSubMenuComp = false;
+
+let HelpresourcesSubMenuComp = document.getElementById('HelpresourcesSubMenuComp');
+let HelpresourcesSubMenuCompArrow = document.getElementById('HelpresourcesSubMenuCompArrow');
+let isOpenHelpresourcesSubMenuComp = false;
+
+let AccountSubMenuComp = document.getElementById('AccountSubMenuComp');
+let AccountSubMenuCompArrow = document.getElementById('AccountSubMenuCompArrow');
+let isOpenAccountSubMenuComp = false;
+
+let LanguageSubMenuComp = document.getElementById('LanguageSubMenuComp');
+let LanguageSubMenuCompArrow = document.getElementById('LanguageSubMenuCompArrow');
+let isOpenLanguageSubMenuComp = false;
+
+const openSubMenuComp = (el) =>{
+  switch (el) {
+    case "MyBusinessSubMenuComp":
+      if (isOpenMyBusinessSubMenuComp) {
+          animacionElSubMenuCompClose(MyBusinessSubMenuComp);
+          MyBusinessSubMenuCompArrow.src="./img/down.png";
+      } else{
+          animacionElSubMenuCompOpen(MyBusinessSubMenuComp);
+          MyBusinessSubMenuCompArrow.src="./img/up.png";
+      }
+    isOpenMyBusinessSubMenuComp = !isOpenMyBusinessSubMenuComp;
+    break;
+    case "GrowthmarketingSubMenuComp":
+      if (isOpenGrowthmarketingSubMenuComp) {
+          animacionElSubMenuCompClose(GrowthmarketingSubMenuComp);
+          GrowthmarketingSubMenuCompArrow.src="./img/down.png";
+      } else{
+          animacionElSubMenuCompOpen(GrowthmarketingSubMenuComp);
+          GrowthmarketingSubMenuCompArrow.src="./img/up.png";
+      }
+    isOpenGrowthmarketingSubMenuComp = !isOpenGrowthmarketingSubMenuComp;
+    break;
+    case "AnalyticsSubMenuComp":
+      if (isOpenAnalyticsSubMenuComp) {
+          animacionElSubMenuCompClose(AnalyticsSubMenuComp);
+          AnalyticsSubMenuCompArrow.src="./img/down.png";
+      } else{
+          animacionElSubMenuCompOpen(AnalyticsSubMenuComp);
+          AnalyticsSubMenuCompArrow.src="./img/up.png";
+      }
+    isOpenAnalyticsSubMenuComp = !isOpenAnalyticsSubMenuComp;
+    break;
+    case "HelpresourcesSubMenuComp":
+      if (isOpenHelpresourcesSubMenuComp) {
+          animacionElSubMenuCompClose(HelpresourcesSubMenuComp);
+          HelpresourcesSubMenuCompArrow.src="./img/down.png";
+      } else{
+          animacionElSubMenuCompOpen(HelpresourcesSubMenuComp);
+          HelpresourcesSubMenuCompArrow.src="./img/up.png";
+      }
+    isOpenHelpresourcesSubMenuComp = !isOpenHelpresourcesSubMenuComp;
+    break;
+    case "AccountSubMenuComp":
+      if (isOpenAccountSubMenuComp) {
+          animacionElSubMenuCompClose(AccountSubMenuComp);
+          AccountSubMenuCompArrow.src="./img/down.png";
+      } else{
+          animacionElSubMenuCompOpen(AccountSubMenuComp);
+          AccountSubMenuCompArrow.src="./img/up.png";
+      }
+    isOpenAccountSubMenuComp = !isOpenAccountSubMenuComp;
+    break;
+    case "LanguageSubMenuComp":
+      if (isOpenLanguageSubMenuComp) {
+          animacionElSubMenuCompClose(LanguageSubMenuComp);
+          LanguageSubMenuCompArrow.src="./img/down.png";
+      } else{
+          animacionElSubMenuCompOpen(LanguageSubMenuComp);
+          LanguageSubMenuCompArrow.src="./img/up.png";
+      }
+    isOpenLanguageSubMenuComp = !isOpenLanguageSubMenuComp;
+    break;
+    default:
+      break;
+  }
+}
+const animacionElSubMenuCompOpen = (el)=>{
+  el.style.display='block';
+  el.style.animationName="openSubMenuComponent";
+  el.style.animationDuration="500ms";
+  el.style.animationFillMode="initial";
+}
+const animacionElSubMenuCompClose = (el)=>{
+  el.style.animationName="closeSubMenuComponent";
+  el.style.animationDuration="500ms";
+  el.style.animationFillMode="initial";
+  setTimeout(() => {      
+      el.style.display='none';
+   }, 400);
+}
---
+++ js/SubMenuExt.js
@@ -0,0 +1,117 @@
+let MyBusinessSubMenu = document.getElementById('MyBusinessSubMenu');
+let isOpenMyBusinessSubMenu = false;
+let GrowthSubMenu = document.getElementById('GrowthSubMenu');
+let isOpenGrowthSubMenu = false;
+let AnalyticsSubMenu = document.getElementById('AnalyticsSubMenu');
+let isOpenAnalyticsSubMenu = false;
+let subMenuDerecha = document.getElementById('subMenuDerecha');
+let isOpensubMenuDerecha = false;
+let QuestionsSubMenu = document.getElementById('QuestionsSubMenu');
+let isOpenQuestionsSubMenu = false;
+let NotificationsEventElementList = document.getElementById('NotificationsEventElementList');
+let isOpenNotificationsEventElementList = false;
+let InboxEventElementList = document.getElementById('InboxEventElementList');
+let isOpenInboxEventElementList = false;
+
+let MyBusinessSubMenuArrow = document.getElementById('MyBusinessSubMenuArrow');
+let GrowthSubMenuArrow = document.getElementById('GrowthSubMenuArrow');
+let AnalyticsSubMenuArrow = document.getElementById('AnalyticsSubMenuArrow');
+const openSubMenu = (el)=>{
+    switch (el) {
+        case 'NotificationsEventElementList':
+            if (isOpenNotificationsEventElementList) {
+                animacionElListClose(NotificationsEventElementList);
+            } else{
+                animacionElListOpen(NotificationsEventElementList);
+            }
+            isOpenNotificationsEventElementList = !isOpenNotificationsEventElementList;
+            break;
+        case 'InboxEventElementList':
+            if (isOpenInboxEventElementList) {
+                animacionElListClose(InboxEventElementList);
+            } else{
+                animacionElListOpen(InboxEventElementList);
+            }
+            isOpenInboxEventElementList = !isOpenInboxEventElementList;
+            break;
+        case 'MyBusinessSubMenu':
+            if (isOpenMyBusinessSubMenu) {
+                animacionElClose(MyBusinessSubMenu);
+                MyBusinessSubMenuArrow.src="./img/down.png";
+            } else{
+                animacionElOpen(MyBusinessSubMenu);
+                MyBusinessSubMenuArrow.src="./img/up.png";
+            }
+            isOpenMyBusinessSubMenu = !isOpenMyBusinessSubMenu;
+            break;
+        case 'GrowthSubMenu':
+            if (isOpenGrowthSubMenu) {
+                animacionElClose(GrowthSubMenu);
+                GrowthSubMenuArrow.src="./img/down.png";
+            } else{
+                animacionElOpen(GrowthSubMenu);
+                GrowthSubMenuArrow.src="./img/up.png";
+            }
+            isOpenGrowthSubMenu = !isOpenGrowthSubMenu;
+            break;
+        case 'AnalyticsSubMenu':
+            if (isOpenAnalyticsSubMenu) {
+                animacionElClose(AnalyticsSubMenu);
+                AnalyticsSubMenuArrow.src="./img/down.png";
+            } else{
+                animacionElOpen(AnalyticsSubMenu);
+                AnalyticsSubMenuArrow.src="./img/up.png";
+            }
+            isOpenAnalyticsSubMenu = !isOpenAnalyticsSubMenu;
+            break;
+        case 'subMenuDerecha':
+            if (isOpensubMenuDerecha) {
+                animacionElClose(subMenuDerecha);
+            } else{
+                animacionElOpen(subMenuDerecha);
+            }
+            isOpensubMenuDerecha = !isOpensubMenuDerecha;
+            break;
+        case 'QuestionsSubMenu':
+            if (isOpenQuestionsSubMenu) {
+                animacionElClose(QuestionsSubMenu);
+            } else{
+                animacionElOpen(QuestionsSubMenu);
+            }
+            isOpenQuestionsSubMenu = !isOpenQuestionsSubMenu;
+            break;
+            
+        default:
+            break;
+    }
+}
+
+const animacionElOpen = (el)=>{
+    el.style.display='block';
+    el.style.animationName="openSubMenu";
+    el.style.animationDuration="500ms";
+    el.style.animationFillMode="initial";
+}
+const animacionElClose = (el)=>{
+    el.style.animationName="closeSubMenu";
+    el.style.animationDuration="500ms";
+    el.style.animationFillMode="initial";
+    setTimeout(() => {      
+        el.style.display='none';
+     }, 400);
+}
+
+const animacionElListOpen = (el)=>{
+    el.style.display='block';
+    el.style.animationName="openEventElementList";
+    el.style.animationDuration="500ms";
+    el.style.animationFillMode="initial";
+}
+const animacionElListClose = (el)=>{
+    el.style.animationName="closeEventElementList";
+    el.style.animationDuration="500ms";
+    el.style.animationFillMode="initial";
+    setTimeout(() => {      
+        el.style.display='none';
+     }, 400);
+}
